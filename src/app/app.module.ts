052cd069cc69272b01a04846d7743c92b54ae12e
Vinodh Kumar BR <vinodh.kumarbr@Tyson.com>
2021-06-27 16:50:55 +0530

    open source repo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { DemoMaterialModule } from './material-module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule, DemoMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
